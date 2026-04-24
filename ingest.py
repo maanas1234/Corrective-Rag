77f5eb5eaecbce7bae8daf9cecf3ddb2420ad2e8
maanas1234 <maanastyagi2006@gmail.com>
2026-04-24 21:41:44 +0530

    added dependencies

---
+++ ingest.py
@@ -0,0 +1,8 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader,TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from openai import OpenAI
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+import os
