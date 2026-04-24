36f0072bb81b7d222f206e23fca6a3365cf18020
maanas1234 <maanastyagi2006@gmail.com>
2026-04-25 00:22:37 +0530

    the query is working properly, we have a proper RAG now.

--- query.py
+++ query.py
@@ -15,3 +15,49 @@ embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-
 vectorstore = FAISS.load_local("vectorstore",embeddings=embeddings,allow_dangerous_deserialization=True)
 retriver = vectorstore.as_retriever(search_type="mmr",search_kwargs={"k":4,"fetch_k":7})
 
+
+llm = ChatOpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key =os.getenv("OPENROUTER_API_KEY"),
+    model="openai/gpt-oss-120b:free"  # free tier model
+)
+
+
+
+prompt = ChatPromptTemplate.from_template("""
+Answer the question using ONLY the context below.
+If the answer is not in the context, say "I don't know."
+(("placeholder", "{chat_history_messages}")
+Context:
+{context}
+
+Question: {question}
+
+Answer:
+""")
+
+
+chat_history_memory = ChatMessageHistory()
+def get_messages(x):
+    return chat_history_memory.messages
+
+
+chain = ({"context": retriver,"question":RunnablePassthrough(),"chat_history_messages":RunnableLambda(get_messages)} | prompt |  llm | StrOutputParser())
+
+
+
+
+response = retriver.invoke("what will happen if we get Ai drivers?")
+print(response)
+
+print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+
+
+result = chain.invoke("what if we have ai drivers?")
+print(result[:500])
