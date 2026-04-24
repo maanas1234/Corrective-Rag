ad53a1c76bbd7a6fafb73c2017c6a7b8a3e5b881
maanas1234 <maanastyagi2006@gmail.com>
2026-04-24 23:16:38 +0530

    coded the ingest.py file

--- ingest.py
+++ ingest.py
@@ -6,3 +6,24 @@ from openai import OpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
 import os
+
+
+loader = DirectoryLoader('./data/notes', glob="**/*.md", loader_cls=TextLoader)
+docs = loader.load()
+
+print(f"Length{len(docs)}")
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    separators=["\n\n", "\n", ".", " ", ""]
+)
+
+embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+chunks = splitter.split_documents(docs)
+                                         
+vectorstore = FAISS.from_documents(chunks, embeddings)
+vectorstore.save_local("vectorstore")
+
+
+print("Done. Index saved to faiss_index/")
