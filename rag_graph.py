8d4b933ee3179ef802466a7b896ce22425858f25
maanas1234 <maanastyagi2006@gmail.com>
2026-04-26 16:31:56 +0530

    websearch node stuff

--- rag_graph.py
+++ rag_graph.py
@@ -1,4 +1,6 @@
 from grader import grade_doc
+from web_search import web_search
+from web_search import web_scrape
 from typing import TypedDict, List
 from langchain_core.documents import Document
 
@@ -15,3 +17,8 @@ def grade_node(state):
     any_bad = any(grade_doc(question, d) == "no" for d in docs)
     return {"go_web": any_bad}
 
+def web_search_node(state):
+    question = state['question']
+    search_result = web_search(question,3)
+    scraped_result = web_scrape(search_result)
+    state['documents'].append(scraped_result)
